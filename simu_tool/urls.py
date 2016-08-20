d9fd764a5664b832263ea10ed8de1b4a1bc73621
huster-wgm <guangmingwu2010@gmail.com>
2016-08-20 20:23:49 +0900

    fix setting

--- simu_tool/urls.py
+++ simu_tool/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
-from . import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/',include('polls.urls')),
-    url(r'^simulator/', include('simulator.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),	
+    url(r'^simulator/', include('simulator.urls')),	
 ]
 
