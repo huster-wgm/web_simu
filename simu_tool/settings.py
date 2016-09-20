d21e8e1860e1eae8a5aece9100942323cd980650
huster-wgm <guangmingwu2010@gmail.com>
2016-09-20 17:11:08 +0900

    reform database setting

--- simu_tool/settings.py
+++ simu_tool/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -102,8 +101,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
