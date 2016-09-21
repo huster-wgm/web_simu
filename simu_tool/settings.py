a2e4397338f6669615ebd6b8fb6b167bcdbb0bc8
huster-wgm <guangmingwu2010@gmail.com>
2016-09-21 11:06:59 +0900

    change database to posgresql

--- simu_tool/settings.py
+++ simu_tool/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -101,6 +102,8 @@ DATABASES = {
     }
 }
 
+if os.getcwd() == "/app":
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
