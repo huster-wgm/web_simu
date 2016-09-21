f47078227d19637d621e45d3dc3a11b8ff836219
HIROAKI GO <guangmingwu2010@gmail.com>
2016-09-21 11:12:44 +0900

    Update settings.py

--- simu_tool/settings.py
+++ simu_tool/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 }
 
 if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+    DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
