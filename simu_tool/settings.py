a081f80ab5db2cc31f1b07db402922e512934ba6
huster-wgm <guangmingwu2010@gmail.com>
2016-09-21 11:19:20 +0900

    setting

--- simu_tool/settings.py
+++ simu_tool/settings.py
@@ -95,15 +95,8 @@ WSGI_APPLICATION = 'simu_tool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),               
-    }
-}
 
-if os.getcwd() == "/app":
-    DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
