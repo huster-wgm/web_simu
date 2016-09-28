f0e59c8dfabdbcb6d76bcb327af247bdea43d4ca
huster-wgm <guangmingwu2010@gmail.com>
2016-09-29 01:17:38 +0900

    add page view in index.html

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    latest_posts = Post.objects.live().order_by('-first_published_at')[:5]
-    context = {'latest_posts': latest_posts}
+    posts = Post.objects.live().order_by('-first_published_at')
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
+    context = {'post_list': post_list}
     return render(request, 'blog/index.html',context)
     
 def categories(request):    
