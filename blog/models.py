e785457de114966757959cbfe3a048d3301d0463
huster-wgm <guangmingwu2010@gmail.com>
2016-10-06 17:37:01 +0900

    add markdown block

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 from wagtail.wagtailsearch import index
 from wagtail.wagtailcore import blocks
@@ -8,13 +9,30 @@ from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailembeds.blocks import EmbedBlock
+
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
+from markdown import markdown
 
 class PageTag(TaggedItemBase):
     content_object = ParentalKey('blog.Post', related_name='tagged_items')
 
+class MarkDownBlock(blocks.TextBlock):
+    """ MarkDown Block """
+
+    class Meta:
+        icon = 'code'
+
+    def render_basic(self, value):
+        md = markdown(
+            value,
+            [
+                'markdown.extensions.fenced_code',
+                'codehilite',
+            ],
+        )
+        return mark_safe(md)
 
 class Post(Page):
     tags = ClusterTaggableManager(through=PageTag, blank=True)
@@ -26,7 +44,8 @@ class Post(Page):
         ('paragraph', blocks.RichTextBlock()),
         ('image', ImageChooserBlock()),
         ('Html_code',blocks.RawHTMLBlock()),
-        ('Embed',EmbedBlock())
+        ('Embed',EmbedBlock()),
+        ('Markdown',MarkDownBlock()),
     ])
 
     search_fields = Page.search_fields + [
